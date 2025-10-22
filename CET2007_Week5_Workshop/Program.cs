c83af88578312dfd5908e9da7c263ad8f7d8990b
NyiKhant1 <nkz.nyikhantzaw@gmail.com>
2025-10-22 10:15:17 +0100

    Exercise 1 done

--- CET2007_Week5_Workshop/Program.cs
+++ CET2007_Week5_Workshop/Program.cs
@@ -8,13 +8,91 @@ namespace CET2007_Week5_Workshop
 {
     internal class Program
     {
+        public class NotificaitonMessage
+        {
+            public string Title { get; set; }
+            public string Message { get; set; }
 
-        /// <summary>
-        /// Factory class responsible for creating notification objects.
-        /// Demonstrates the Factory Design Pattern.
-        /// </summary>
+            public override string ToString()
+            {
+                return $"Reminder: {Title} - {Message}";
+            }
+        }
+        public interface IReminderChannel
+        {
+            void Send(string recipient, NotificaitonMessage Message );
+        }
+        public class EmailChannel : IReminderChannel
+        {
+            public void Send(string recipient, NotificaitonMessage Message )
+            {
+                Console.WriteLine($"Email sent to {recipient} : {Message.Title} - {Message.Message}");
+            }
+        }
+        public class SmsChannel : IReminderChannel
+        {
+            public void Send (string recipient, NotificaitonMessage Message )
+            {
+                Console.WriteLine($"SMS sent to {recipient} : {Message.Title} - {Message.Message}");
+            }
+        }
+        public class NoticeBoardChannel : IReminderChannel
+        {
+            public void Send(string recipient, NotificaitonMessage Message)
+            {
+                Console.WriteLine($"Notice posted at {recipient} : {Message.Title} - {Message.Message}");
+            }
+        }
+        public class ReminderFactory
+        {
+            public  IReminderChannel CreateReminder (string choice)
+            {
+                if (choice.ToLower() == "email")
+                {
+                    return new EmailChannel();
+                }
+                else if (choice.ToLower() == "sms")
+                {
+                     return new SmsChannel();
+                }
+                else if (choice.ToLower() == "noticeboard")
+                {
+                    return new NoticeBoardChannel();    
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid notification type" + choice);
+                }    
+            }
+                
+        }
         static void Main(string[] args)
         {
+            ReminderFactory Factory = new ReminderFactory();
+
+            IReminderChannel emailchannel = Factory.CreateReminder("email");
+            IReminderChannel smschannel = Factory.CreateReminder("sms");
+            IReminderChannel noticeboardchannel = Factory.CreateReminder("noticeboard");
+
+            NotificaitonMessage reminder = new NotificaitonMessage()
+            {
+                Title = "Book due soon",
+                Message = "Please return 'Object-Oriented Design' by Friday 5pm "
+            };
+
+            IReminderChannel [] channel = {emailchannel, smschannel, noticeboardchannel};
+
+            string[] recipents = { "Alice", "Bob", "Main Library Foyer" };
+
+            for (int i = 0; i < recipents.Length; i++)
+            {
+                channel[i].Send(recipents[i], reminder);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Formated Message" + reminder.ToString());
+
+            
         }
     }
 }
