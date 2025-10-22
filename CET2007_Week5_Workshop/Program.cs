75242379bf5338d4901f843ea5eaa8003a23ae0d
NyiKhant1 <nkz.nyikhantzaw@gmail.com>
2025-10-22 09:27:17 +0100

    Add project files.

---
+++ CET2007_Week5_Workshop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CET2007_Week5_Workshop
+{
+    internal class Program
+    {
+
+        /// <summary>
+        /// Factory class responsible for creating notification objects.
+        /// Demonstrates the Factory Design Pattern.
+        /// </summary>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
