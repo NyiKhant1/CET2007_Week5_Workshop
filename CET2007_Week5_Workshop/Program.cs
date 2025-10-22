6ae53aecb65d7a0409864fff7572774f2f31a317
NyiKhant1 <nkz.nyikhantzaw@gmail.com>
2025-10-22 11:21:26 +0100

    Exercise 2 Done

--- CET2007_Week5_Workshop/Program.cs
+++ CET2007_Week5_Workshop/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CET2007_Week5_Workshop
 {
@@ -64,7 +67,66 @@ namespace CET2007_Week5_Workshop
                     throw new ArgumentException("Invalid notification type" + choice);
                 }    
             }
-                
+        }
+        public interface IObserver
+        {
+            void Update(NotificaitonMessage message);
+            string Name { get; }
+        }
+        public class MemberObserver : IObserver
+        {
+            public string Name { get; private set; }
+            private IReminderChannel reminderChannel;
+
+            public MemberObserver (string name, IReminderChannel channel)
+            {
+                Name = name;
+                reminderChannel = channel;
+            }
+            public void Update(NotificaitonMessage message)
+            {
+                reminderChannel.Send(Name,message);
+            }
+        }
+        public class StaffObserver : IObserver
+        {
+            public string Name { get; private set; }
+            private IReminderChannel reminderChannel;
+
+            public StaffObserver(string name, IReminderChannel channel)
+            {
+                Name = name;
+                reminderChannel = channel;
+            }
+            public void Update(NotificaitonMessage message)
+            {
+                reminderChannel.Send(Name, message);
+            }
+        }
+        public class LibraryPortal
+        {
+            private IObserver[] observers;
+            private int count;
+            public LibraryPortal(int maxObservers)
+            {
+                observers = new IObserver[maxObservers];
+                count = 0;
+            }
+            public void Register(IObserver observer)
+            {
+                if (count < observers.Length) 
+                {
+                    observers[count] = observer;
+                    count++;
+                }
+            }
+            public void BoradCast(NotificaitonMessage message)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    observers[i].Update(message);
+                }
+            }
         }
         static void Main(string[] args)
         {
@@ -82,7 +144,7 @@ namespace CET2007_Week5_Workshop
 
             IReminderChannel [] channel = {emailchannel, smschannel, noticeboardchannel};
 
-            string[] recipents = { "Alice", "Bob", "Main Library Foyer" };
+            string[] recipents = { "Alice", "Bob", "Main Library" };
 
             for (int i = 0; i < recipents.Length; i++)
             {
@@ -92,7 +154,22 @@ namespace CET2007_Week5_Workshop
             Console.WriteLine("");
             Console.WriteLine("Formated Message" + reminder.ToString());
 
-            
+            LibraryPortal portal = new LibraryPortal(10);
+            MemberObserver alice = new MemberObserver("Alice", emailchannel);
+            MemberObserver bob = new MemberObserver("Bob", smschannel);
+            StaffObserver john = new StaffObserver("John", noticeboardchannel);
+
+            portal.Register(alice);
+            portal.Register(bob);
+            portal.Register(john);
+            NotificaitonMessage reminder1 = new NotificaitonMessage()
+            {
+                Title = "Book Due Tommorow",
+                Message = "Please return ‘Design Patterns’ by 5 PM."
+            };
+
+            portal.BoradCast(reminder1);
         }
     }
 }
+ 
